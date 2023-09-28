b0d45f5704578b3c2966deb30ce7c928e23dddb1
Dacobits <103399198+Dacobits@users.noreply.github.com>
2023-09-28 10:19:23 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// En este archivo no utilizamos el evento "DOMContentLoaded", ya que se colocó el atributo "defer" en la importación del script,
+// que nos soluciona el problema de los elementos no cargados del DOM. Más info => https://www.w3schools.com/tags/att_script_defer.asp
+
+const DATA_URL = "json/data.json"
+
+const container = document.getElementById("container"); // "Traemos" utilizando el DOM el div de id "container" para colocar la información en él
+
+/**
+ * Función que recibe por parámetro un array con los datos que se mostrarán en el DOM
+ * Los datos se mostrarán dentro del div de id "container" y por cada ítem se está creando un nuevo párrafo donde se
+ * imprime el campo "name" y el campo "lastname" separados por un espacio
+ */
+function showData(dataArray) {
+  // El for itera sobre los elementos del array
+  for (const item of dataArray) {
+    // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
+    container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
+  }
+}
+
+// Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+fetch(DATA_URL)
+  .then((res) => res.json())
+  .then(data => showData(data.students))
+  .catch(console.error)
+
+
+/*PRUEBA DE ASYNC AWAIT
+
+async function getData(){
+  let resp = await fetch(DATA_URL);
+  let data = await resp.json();
+  console.log(data);
+  return showData(data.students)
+}
+
+document.addEventListener(`DOMContentLoaded`, ()=>{
+    getData();
+})*/
+
